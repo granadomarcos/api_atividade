5e7eee0c27cad1f0205b76553ee3db56fac9b024
Marcos Granado <granado.rodrigues@gmail.com>
2020-07-05 12:51:09 -0300

    Alterado a nossa REST API para realizar autenticação no banco de dados.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from models import Pessoas
+from models import Pessoas, Usuarios
 
 def insere_pessoas():
     pessoa = Pessoas(nome='Vinicius', idade=26)
@@ -19,9 +19,21 @@ def exclui_pessoa():
     pessoa = Pessoas.query.filter_by(nome='Vinicius').first()
     pessoa.delete()
 
+def insere_usuario(login, senha):
+    usuario = Usuarios(login=login, senha=senha)
+    usuario.save()
+
+def consulta_todos_usuarios():
+    usuarios = Usuarios.query.all()
+    print(usuarios)
+
+
 if __name__=='__main__':
+    insere_usuario('granado', '1234')
+    insere_usuario('rodrigues', '4321')
+    consulta_todos_usuarios()
     #insere_pessoas()
     #altera_pessoa()
-    exclui_pessoa()
-    consulta_pessoas()
+    #exclui_pessoa()
+    #consulta_pessoas()
 
